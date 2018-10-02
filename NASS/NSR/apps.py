23009dbcebfa0cc5c4c12b9e6eea15ec99c87b55
root <root@ip-172-31-19-16.us-east-2.compute.internal>
2018-10-02 12:12:38 -0500

    Got all three pages done - backup before configuring apps

--- NASS/NSR/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NsrConfig(AppConfig):
-    name = 'NSR'
--- NASS/NSR/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='NSR-Home'),
-    #path('/', views.home, name='NSR-Home'),
-	# add other paths here
-]
